416266c7d1a1809c807277040128da026ea630a3
Avuluri Venkatas Sai Reddy <avulurivenkatasaireddy@gmail.com>
2019-05-11 08:06:30 +0530

    Add files via upload

---
+++ firstprogram/src/HelloWorld.scala
@@ -0,0 +1,12 @@
+
+
+object HelloWorld {
+  def main(args: Array[String]) {
+    val name="Sai"
+    val age=18
+    println("Hello, world!")
+    println(name+" is "+ age + " yeras old")
+    println(s"$name is $age years old")
+     println(f"$name%s is $age%d years old")
+  }
+}
